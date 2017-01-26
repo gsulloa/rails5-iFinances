ea617a601213b80d84e618bfafbd9ccd3b8ad885
Gabriel Ulloa <gsulloa@uc.cl>
2017-01-26 01:54:21 -0300

    Creado seed con admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+admin = User.create(email: 'admin@ifinances.io', password: '1q2w3e4r5tifinances', role: 1)
