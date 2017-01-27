2c629cd36413fb06ef6b7a8960a3556ad2e00885
Gabriel Ulloa <gsulloa@uc.cl>
2017-01-27 17:14:22 -0300

    Arreglada migracion

--- db/migrate/20170126045756_add_default_to_cdefault.rb
+++ db/migrate/20170126045756_add_default_to_cdefault.rb
@@ -1,9 +1,9 @@
 class AddDefaultToCdefault < ActiveRecord::Migration[5.0]
   def up
-		change_column_default :cdefaults, :default, from: nil, to: 0
+		change_column_default :cdefaults, :default, from: nil, to: false
 	end
 
 	def down
-  		change_column_default :cdefaults, :default, from: nil, to: 0
+  		change_column_default :cdefaults, :default, from: false, to: nil
 	end
 end
